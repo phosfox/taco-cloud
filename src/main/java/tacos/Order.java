48029b4b7b0596a40ae99906a3f626757bf19e2a
phosfox <mordredur@googlemail.com>
2020-11-18 20:20:58 +0100

    at chapter 3.2

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -6,23 +6,55 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Data
 public class Order {
-    @NotBlank(message = "Name is required")
-    private String name;
+
+    private Long id;
+
+    private Date placedAt;
+
+//end::newFields[]
+
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryName;
+
     @NotBlank(message = "Street is required")
-    private String street;
+    private String deliveryStreet;
+
     @NotBlank(message = "City is required")
-    private String city;
+    private String deliveryCity;
+
     @NotBlank(message = "State is required")
-    private String state;
-    @NotBlank(message = "Zip is required")
-    private String zip;
+    private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
+    private String deliveryZip;
+
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([/])([1-9][0-9])$", message = "Must be formatted MM/YY")
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message = "Must be formatted MM/YY")
     private String ccExpiration;
+
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+  /*
+// tag::newFields[]
+  ...
+// end::newFields[]
+   */
+//tag::newFields[]
 }
+//end::newFields[]
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,59 +1,126 @@
 package tacos.web;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import lombok.extern.slf4j.Slf4j;
-import tacos.Design;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import tacos.Taco;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
+import tacos.data.TacoRepository;
+import tacos.data.IngredientRepository;
 
-import javax.validation.Valid;
-
-@Slf4j
+// tag::classShell[]
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+
+//end::classShell[]
+
+    //tag::bothRepoProperties[]
+//tag::ingredientRepoProperty[]
+    private final IngredientRepository ingredientRepo;
+
+    //end::ingredientRepoProperty[]
+    private TacoRepository designRepo;
+
+//end::bothRepoProperties[]
+
+  /*
+// tag::ingredientRepoOnlyCtor[]
+  @Autowired
+  public DesignTacoController(IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
+// end::ingredientRepoOnlyCtor[]
+   */
+
+    //tag::bothRepoCtor[]
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository designRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    //end::bothRepoCtor[]
+
+    // tag::modelAttributes[]
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    // end::modelAttributes[]
+    // tag::showDesignForm[]
+
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
-                    ingredients.stream().filter(ingredient -> ingredient.getType() == type).collect(Collectors.toList()));
+                    filterByType(ingredients, type));
         }
-        model.addAttribute("design", new Taco());
+
         return "design";
     }
+//end::showDesignForm[]
 
+    //tag::processDesign[]
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(
+            @Valid Taco design, Errors errors,
+            @ModelAttribute Order order) {
+
         if (errors.hasErrors()) {
             return "design";
         }
-        // in chap 3
-        log.info("Processing design: " + design);
+
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
 
         return "redirect:/orders/current";
     }
-}
+    //end::processDesign[]
 
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+  /*
+//tag::classShell[]
+  ...
+//end::classShell[]
+   */
+//tag::classShell[]
+
+}
+//end::classShell[]
